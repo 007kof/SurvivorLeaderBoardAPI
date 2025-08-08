7da2965d0ba77c56394010dc28ba4d701df37fdc
007kof <alex.ji.chen@gmail.com>
2025-08-08 19:17:08 +0200

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/leaderboard');
+    await mongoose.connect('mongodb+srv://007kof:Alexjichen98@game.eyf1pdg.mongodb.net/leaderboard')
+    // await mongoose.connect('mongodb://localhost:27017/leaderboard');
     console.log('MongoDB connected');
   } catch (error) {
     console.error(error);
